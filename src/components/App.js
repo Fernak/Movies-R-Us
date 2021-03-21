996fdbb3a4b3f6addc38594788e4eb6d588388c4
Fernak <ndeutsch29@gmail.com>
2021-03-21 14:03:43 -0600

    Auth System Done

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import PrivateRoute from "./PrivateRoute";
+
+import Dashboard from "./dashboard/Dashboard"
+import SignUp from "./login/SignUp"
+import Login from "./login/Login"
+import ForgotPassword from "./login/ForgotPassword"
+
+function App() {
+  return (
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: '400px' }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+  )
+}
+
+export default App;
